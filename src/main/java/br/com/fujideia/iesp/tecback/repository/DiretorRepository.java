f56c6348f8a4064cbd734c0fa52cbc8e6644f60d
Clara Ferraz <claraferraz@proton.me>
2024-09-14 16:12:33 -0300

    Diretor controller, dto e repository

--- src/main/java/br/com/fujideia/iesp/tecback/repository/DiretorRepository.java
+++ src/main/java/br/com/fujideia/iesp/tecback/repository/DiretorRepository.java
@@ -1,4 +1,13 @@
 package br.com.fujideia.iesp.tecback.repository;
 
-public interface DiretorRepository {
+import br.com.fujideia.iesp.tecback.model.Diretor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface DiretorRepository extends JpaRepository<Diretor, Long> {
+    @Query("SELECT d FROM Diretor d WHERE d.nome LIKE %:nome%")
+    List<Diretor> buscarPorNome(@Param("nome") String nome);
 }
