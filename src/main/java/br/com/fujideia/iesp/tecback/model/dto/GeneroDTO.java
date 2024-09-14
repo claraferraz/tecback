a9878569d5c7c0ae4fbe42d048c80f83b732f21f
Clara Ferraz <claraferraz@proton.me>
2024-09-14 16:26:15 -0300

    genero repository, controller e service

--- src/main/java/br/com/fujideia/iesp/tecback/model/dto/GeneroDTO.java
+++ src/main/java/br/com/fujideia/iesp/tecback/model/dto/GeneroDTO.java
@@ -10,5 +10,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class GeneroDTO {
     private Long id;
-    private String descricao;
+    private String nome;
 }
--- src/main/java/br/com/fujideia/iesp/tecback/repository/GeneroRepository.java
+++ src/main/java/br/com/fujideia/iesp/tecback/repository/GeneroRepository.java
@@ -1,4 +1,13 @@
 package br.com.fujideia.iesp.tecback.repository;
 
-public interface GeneroRepository {
+import br.com.fujideia.iesp.tecback.model.Genero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface GeneroRepository extends JpaRepository<Genero, Long> {
+    @Query("SELECT g FROM Genero g WHERE g.descricao LIKE %:descricao%")
+    List<Genero> buscarPorDescricao(@Param("descricao") String descricao);
 }
